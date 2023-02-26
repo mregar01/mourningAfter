ea1a09f7dde045926825b3c0cfcbe8087743aafe
mregar01 <max.regardie@tufts.edu>
2023-02-25 20:57:14 -0500

    added timer

--- MourningAft/Assets/GameHandler.cs
+++ MourningAft/Assets/GameHandler.cs
@@ -5,20 +5,45 @@ using UnityEngine.UI;
 
 public class GameHandler : MonoBehaviour {
 
-      public GameObject scoreText;
-      private int playerScore = 0;
+        public GameObject scoreText;
+        public GameObject timeText;
+        public GameObject gameOverText;
+        private int playerScore = 0;
 
-      void Start(){
-            UpdateScore();
-      }
+        public int gameTime = 20;
+        private float gameTimer = 0f;
 
-      public void AddScore(int points){
-            playerScore += points;
-            UpdateScore();
-      }
+        void Start(){
+                UpdateScore();
+                gameOverText.SetActive(false); 
+                UpdateTime(); 
+        }
 
-      void UpdateScore(){
-            Text scoreTextB = scoreText.GetComponent<Text>();
-            scoreTextB.text = "" + playerScore;
-      }
+        public void AddScore(int points){
+                playerScore += points;
+                UpdateScore();
+        }
+
+        void FixedUpdate(){
+                gameTimer += 0.01f;
+                if (gameTimer >= 1f){
+                                gameTime -= 1;
+                                gameTimer = 0;
+                                UpdateTime();
+                }
+                if (gameTime <= 0){ 
+                                gameTime = 0; 
+                                gameOverText.SetActive(true);
+                }
+        }
+
+        void UpdateScore(){
+                Text scoreTextB = scoreText.GetComponent<Text>();
+                scoreTextB.text = "" + playerScore;
+        }
+        
+        public void UpdateTime(){
+                Text timeTextB = timeText.GetComponent<Text>();
+                timeTextB.text = "" + gameTime;
+        }
 }
